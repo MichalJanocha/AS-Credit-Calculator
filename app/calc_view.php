20777a5e843df4e29a25e8ed9d50a3bc12b3054e
Michal <michal.janocha@symfoni.com>
2021-10-10 18:21:38 +0200

    View, Controller

---
+++ app/calc_view.php
@@ -0,0 +1,41 @@
+<?php require_once dirname(__FILE__) .'/../config.php';?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator kredytowy</title>
+</head>
+<body>
+<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
+	<label for="amount">Kwota (PLN): </label>
+	<input id="amount" type="number" name="amount" value="<?php isset($amount) ? print($amount) : printf('') ?>" /><br />
+
+	<label for="years">Na ile lat: </label>
+	<input id="years" type="number" name="years" value="<?php isset($years) ? print($years) : printf('') ?>" /><br />
+
+  <label for="percent">Oprocentowanie (%): </label>
+	<input id="percent" type="number" name="percent" value="<?php isset($percent) ? print($percent) : printf('') ?>" /><br />
+
+	<input type="submit" value="Oblicz" />
+</form>	
+
+<?php
+if (isset($errors)) {
+	if (count ( $errors ) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		foreach ( $errors as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+<?php if (isset($result)){ ?>
+<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:350px;">
+<?php echo 'Miesieczna rata to: <strong>'.$result.'</strong>'; ?>
+</div>
+<?php } ?>
+
+</body>
+</html>
---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// const _SERVER_NAME = 'localhost:80';
+// const _SERVER_URL = 'http://' . _SERVER_NAME;
+// const _APP_ROOT = '/as-credit-calculator'; // Using IntelliJ Built-in web server, that's why this var is empty, equals to current path
+// const _APP_URL = _SERVER_URL . _APP_ROOT;
+// define("_ROOT_PATH", dirname(__FILE__));
+const _SERVER_NAME = 'localhost:8080';
+const _SERVER_URL = 'http://' . _SERVER_NAME;
+const _APP_ROOT = ''; // Using IntelliJ Built-in web server, that's why this var is empty, equals to current path
+const _APP_URL = _SERVER_URL . _APP_ROOT;
+define("_ROOT_PATH", dirname(__FILE__));
